01b4f4daed307c74f394be3d98acc77795023547
DarthMaras <114573286+DarthMaras@users.noreply.github.com>
2022-09-28 10:39:17 +0100

    Add files via upload

---
+++ Introduction to Python.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 28 09:52:16 2022
+
+@author: Andreas Maragkos
+"""
+
+print("Hello world") #print a text
+print(2+2) #add two numbers 
+print(2*4) #multiple two numbers
+print(9/3) #division operation
+# Miles conversion factor 
+m_fac=0.62137119
+# assaignment variable to save kilometres value (it could be user input data..)
+kilometers=36
+#calculate miles (this cude is written by functions and could be printed in..)
+miles=kilometers*m_fac
+print("kilometers to miles:",miles)
